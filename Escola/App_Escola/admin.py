df9379767f442a10101e852c8aa098d4f38de206
Isabelli Maciel Marques <44590917807@>
2024-03-21 11:23:51 -0300

    instrucoes sql

--- Escola/App_Escola/admin.py
+++ Escola/App_Escola/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Professor, Turma, Atividade
 
 # Register your models here.
+admin.site.register(Professor)
+admin.site.register(Turma)
+admin.site.register(Atividade)
--- Escola/App_Escola/views.py
+++ Escola/App_Escola/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Professor, Turma, Atividade
+from django.db import connection, transaction
+from django.contrib import messages
+from hashlib import sha256
 
 def abre_index(request):
     return render(request, 'login.html')
@@ -6,3 +10,27 @@ def abre_index(request):
 def enviar_login(request):
     return render(request,'dados_ok.html')
 
+def initial_population():
+    print('Vou popular')
+    cursor = connection.cursor()
+
+    senha = "123456"
+    senha_armazenar = sha256(senha.encode()).hexdigest()
+
+    insert_sql_professor = "INSERT INTO App_SimSAEP_professor (nome, email, senha) VALUES"
+    insert_sql_professor = insert_sql_professor + "('Prof. Barak Obama', 'barak.obama@gmail.com', '"+ senha_armazenar + "'),"
+    insert_sql_professor = insert_sql_professor + "('Profa. Angela Merkel', 'angela.merkel@gmail.com', '"+ senha_armazenar + "'),"
+    insert_sql_professor = insert_sql_professor + "('Prof. Xi Jinping', 'xi.jinping@gmail.com', '"+ senha_armazenar + "'),"
+
+    cursor.execute(insert_sql_professor)
+    transaction.atomic()
+
+    insert_sql_turma = "INSERT INTO App_SimSAEP_turma (nome_turma, id_professor_id) VALUES"
+    insert_sql_turma = insert_sql_turma + "('1o Semestre - Desenvolvimento de Sistemas',1),"
+    insert_sql_turma = insert_sql_turma + "('2o Semestre - Desenvolvimento de Sistemas',2),"
+    insert_sql_turma = insert_sql_turma + "('3o Semestre - Desenvolvimento de Sistemas',3),"
+
+    cursor.execute(insert_sql_turma)
+    transaction.atomic()
+
+    
