cf93b7193a7789b576cf6122d9d4acc8a3a981f6
Isabelli Maciel Marques <44590917807@>
2024-04-04 11:16:36 -0300

    cad atv

--- Escola/App_Escola/views.py
+++ Escola/App_Escola/views.py
@@ -5,6 +5,41 @@ from django.contrib import messages
 from hashlib import sha256
 from django.http import HttpResponse
 
+
+def initial_population():
+    print('Vou popular')
+    cursor = connection.cursor()
+
+    senha = "123456"
+    senha_armazenar = sha256(senha.encode()).hexdigest()
+
+    insert_sql_professor = "INSERT INTO App_Escola_professor (nome, email, senha) VALUES "
+    insert_sql_professor = insert_sql_professor + "('Prof. Barak Obama', 'barak.obama@gmail.com', '"+ senha_armazenar + "'),"
+    insert_sql_professor = insert_sql_professor + "('Profa. Angela Merkel', 'angela.merkel@gmail.com', '"+ senha_armazenar + "'),"
+    insert_sql_professor = insert_sql_professor + "('Prof. Xi Jinping', 'xi.jinping@gmail.com', '"+ senha_armazenar + "')"
+
+    cursor.execute(insert_sql_professor)
+    transaction.atomic()
+
+    insert_sql_turma = "INSERT INTO App_Escola_turma (nome_turma, id_professor_id) VALUES"
+    insert_sql_turma = insert_sql_turma + "('1o Semestre - Desenvolvimento de Sistemas',1),"
+    insert_sql_turma = insert_sql_turma + "('2o Semestre - Desenvolvimento de Sistemas',2),"
+    insert_sql_turma = insert_sql_turma + "('3o Semestre - Desenvolvimento de Sistemas',3)"
+
+    cursor.execute(insert_sql_turma)
+    transaction.atomic()
+
+    insert_sql_atividade = "INSERT INTO App_Escola_atividade (nome_atividade, id_turma_id) VALUES "
+    insert_sql_atividade = insert_sql_atividade + "('Apresentar Fundamentos da Programação', 1),"
+    insert_sql_atividade = insert_sql_atividade + "('Apresentar Framework Django', 2),"
+    insert_sql_atividade = insert_sql_atividade + "('Apresentar conceitos de Gerenciamento de Projetos', 3)"
+
+    cursor.execute(insert_sql_atividade)
+    transaction.atomic()
+
+    print('Populei')
+    
+
 def abre_index(request):
     dado_pesquisa = 'Obama'
     verifica_populado = Professor.objects.filter(nome__icontains=dado_pesquisa)
@@ -30,11 +65,20 @@ def enviar_login(request):
             senha = senha['senha']
             usuario_logado = dados_professor[0]
             usuario_logado = usuario_logado['nome']
+           
+
             if (senha == senha_criptografada):
                 messages.info(request, 'Bem vindo.')
-
-                mensagem = "Olá professor, " + email + "Seja bem vindo!!"
-                return HttpResponse(mensagem) 
+                id_logado = dados_professor[0]
+                id_logado = id_logado['id']
+                turmas_do_professor = Turma.objects.filter(id_professor=id_logado)
+                print("Turma do professor: ", turmas_do_professor)
+                return render(request, 'Cons_Turma_Lista.html',{
+                    'usuario logado': usuario_logado, 'turmas_do professor': turmas_do_professor, 'id_logado': id_logado
+                })
+                # mensagem = "Olá professor, " + email + "Seja bem vindo!!"
+                # # return render(request, 'index.html')
+                # return HttpResponse(mensagem) 
             else: 
                 messages.info(request, 'Usuário ou senha incorretos. Tente Novamente.')
                 return render(request, 'login.html')
@@ -56,42 +100,9 @@ def confirmar_cadastro(request):
         )
         grava_professor.save()
 
-        mensagem = "Olá Professor " + nome + ", Seja bem-vindo!"
-        return HttpResponse(mensagem)
+        return render(request, 'index.html')
 
 
-def initial_population():
-    print('Vou popular')
-    cursor = connection.cursor()
 
-    senha = "123456"
-    senha_armazenar = sha256(senha.encode()).hexdigest()
-
-    insert_sql_professor = "INSERT INTO App_Escola_professor (nome, email, senha) VALUES"
-    insert_sql_professor = insert_sql_professor + "('Prof. Barak Obama', 'barak.obama@gmail.com', '"+ senha_armazenar + "'),"
-    insert_sql_professor = insert_sql_professor + "('Profa. Angela Merkel', 'angela.merkel@gmail.com', '"+ senha_armazenar + "'),"
-    insert_sql_professor = insert_sql_professor + "('Prof. Xi Jinping', 'xi.jinping@gmail.com', '"+ senha_armazenar + "'),"
-
-    cursor.execute(insert_sql_professor)
-    transaction.atomic()
-
-    insert_sql_turma = "INSERT INTO App_Escola_turma (nome_turma, id_professor_id) VALUES"
-    insert_sql_turma = insert_sql_turma + "('1o Semestre - Desenvolvimento de Sistemas',1),"
-    insert_sql_turma = insert_sql_turma + "('2o Semestre - Desenvolvimento de Sistemas',2),"
-    insert_sql_turma = insert_sql_turma + "('3o Semestre - Desenvolvimento de Sistemas',3),"
-
-    cursor.execute(insert_sql_turma)
-    transaction.atomic()
-
-    insert_sql_atividade = "INSERT INTO App_Escola_atividade (nome_atividade, id_turma_id) VALUES"
-    insert_sql_atividade = insert_sql_atividade + "('Apresentar Fundamentos da Programação', 1),"
-    insert_sql_atividade = insert_sql_atividade + "('Apresentar Framework Django', 2),"
-    insert_sql_atividade = insert_sql_atividade + "('Apresentar conceitos de Gerenciamento de Projetos', 3)"
-
-    cursor.execute(insert_sql_atividade)
-    transaction.atomic()
-
-    print('Populei')
-    
 
     
